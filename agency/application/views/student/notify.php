698c52f5bdcf0d1a78e70cf8c64e01bd7cce9c61
zhangys <907628858@qq.com>
2014-10-12 16:33:20 +0800

    sms

--- agency/application/views/student/notify.php
+++ agency/application/views/student/notify.php
@@ -117,7 +117,12 @@ $(function(){
 		$.post(url, {id:$('#student_id').val(), phones:phones.join(',')}, function (jsonStr) {
 			var jsonObj = $.parseJSON(jsonStr);
 			if ( jsonObj.ret != 0 ) {
-				alert(jsonObj.msg);
+				if ( jsonObj.msg['0'] != undefined ) {
+					alert(jsonObj.msg['0']);
+				} else {
+					alert(jsonObj.msg);
+				}
+				
 				return false;
 			}
 			alert('发送成功');
